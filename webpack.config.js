63f019206988cd608995598840b93a78e2ca5469
Aarón García Hervás <aar.garc.herv@gmail.com>
2021-03-31 11:06:30 +0200

    webpack 5 & update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-/* eslint-disable */
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
@@ -10,8 +10,8 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    ui: './src/ui/ui.ts', // The entry point for your UI code
-    plugin: './src/plugin/plugin.ts', // The entry point for your plugin code
+    ui: './src/ui.ts', // The entry point for your UI code
+    plugin: './src/plugin.ts', // The entry point for your plugin code
   },
 
   module: {
@@ -20,10 +20,10 @@ module.exports = (env, argv) => ({
       { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ },
 
       // Enables including CSS by doing "import './file.css'" in your TypeScript code
-      { test: /\.css$/, loader: [{ loader: 'style-loader' }, { loader: 'css-loader' }] },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
 
       // Allows you to use "<%= require('./file.svg') %>" in your HTML code to get a data URI
-      { test: /\.(png|jpg|gif|webp|svg|zip)$/, loader: [{ loader: 'url-loader' }] },
+      { test: /\.(png|jpg|gif|svg|webp)$/, type: 'asset/inline' },
     ],
   },
 
@@ -38,11 +38,11 @@ module.exports = (env, argv) => ({
   // Tells Webpack to generate "ui.html" and to inline "ui.ts" into it
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/ui/ui.html',
+      template: './src/ui.html',
       filename: 'ui.html',
       inlineSource: '.(js)$',
       chunks: ['ui'],
     }),
-    new HtmlWebpackInlineSourcePlugin(),
+    new HtmlInlineScriptPlugin([/ui.js/]),
   ],
 });
